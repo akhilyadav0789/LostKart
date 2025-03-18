b117e949b9a71fec226083976092ca8781974eb7
lostkart <akhilyadav0789@gmail.com>
2025-03-18 13:33:46 +0530

    LostKart

---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+
+function Header() {
+  return (
+    <div>
+        
+        <div>
+            LOST PRODUCT IN YOUR CITY ...in your city
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Header;
