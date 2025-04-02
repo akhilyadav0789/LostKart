b7f45d44eee8f1cd246a04a4af685130dd092e7f
lostkart <akhilyadav0789@gmail.com>
2025-04-02 15:28:58 +0530

    LostKart

--- node-app/index.js
+++ node-app/index.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017', (err,db) => {
+  if (!err) {
+  console.log('Connected to MongoDB!');
+  }
+
+});
+
+
+const User = mongoose.model('Users', { username: String, password: String });
 
 app.get('/', (req, res) => {
   res.send('Hello....')
 })
+
+app.get('signup', (req, res) => {
+     const username = req.body.username;
+     const password = req.body.password;
+     const user = new User({ username: 'usernameee', password: 'passworddd' });
+     user.save().then(() => console.log('saved'));
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
