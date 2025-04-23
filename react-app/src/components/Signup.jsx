af07c62c1c650936f3920bf7d01734bf2506c164
lostkart <akhilyadav0789@gmail.com>
2025-04-23 15:37:11 +0530

    Initial commit

--- react-app/src/components/Signup.jsx
+++ react-app/src/components/Signup.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
+import axios from 'axios';
 
 
 
 function Signup() {
 
-   const [username, setusername] = useState('');
-   console.log(username);
+    const [username, setusername] = useState('');
+    const [password, setpassword] = useState('');
+   
+
+    const handleApi = () => {
+      console.log({username, password});
+      const url = 'http://localhost:4000/signup';
+      const data = { username, password };
+      axios.post(url, data)
+       .then((res) => {
+          console.log(res)
+       })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+
   return (
     <div>
       <Header />
@@ -20,9 +37,13 @@ function Signup() {
          }}  />
          <br></br>
          PASSWORD
-         <input type="password" />
+         <input type="password" value={password}
+          onChange={(e) => {
+              setpassword(e.target.value);
+          }}  />
+         
             <br></br>
-            <button> SIGNUP </button>
+            <button onClick={handleApi}> SIGNUP </button>
           <Link to="/login"> LOGIN </Link>
 
     </div>
