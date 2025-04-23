1a2fd4bec1a8252ba7933dbdf68c771400c6f696
lostkart <akhilyadav0789@gmail.com>
2025-04-23 14:03:18 +0530

    Initial commit

--- node-app/index.js
+++ node-app/index.js
@@ -9,16 +9,24 @@ mongoose.connect('mongodb+srv://LostKart:lostkart123@cluster0.d3b0w.mongodb.net/
 
 const User = mongoose.model('Users', { username: String, password: String });
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   res.send('Hello....')
 })
 
 app.get('signup', (req, res) => {
-    // const username = req.body.username;
-     //const password = req.body.password;
-     const user = new User({ username: 'usernameee', password: 'passworddd' });
-     user.save().then(() => console.log('saved'));
-  })
+    const username = req.body.username;
+    const password = req.body.password;
+     const user = new User({ username: 'usernameee', password: 'passwoddd' });
+     user.save()
+     .then(() => {
+      res.send({message: 'saved successfully'})
+     })
+     .catch(() => {
+      res.send({message: 'error'})
+     })
+  
+    })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- react-app/src/components/Signup.jsx
+++ react-app/src/components/Signup.jsx
@@ -1,16 +1,23 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 
 
 
 function Signup() {
+
+   const [username, setusername] = useState('');
+   console.log(username);
   return (
     <div>
       <Header />
       Welcome to the signup page!
       <br></br>
          USERNAME
-         <input type="text" />
+         <input type="text" value={username}
+         onChange={(e) => {
+            setusername(e.target.value);
+         }}  />
          <br></br>
          PASSWORD
          <input type="password" />
