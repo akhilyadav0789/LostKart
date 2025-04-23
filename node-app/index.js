caa97a5cec1675540a446dbdc9f039d2c1402fa7
lostkart <akhilyadav0789@gmail.com>
2025-04-23 11:49:56 +0530

    Initial commit

--- node-app/index.js
+++ node-app/index.js
@@ -4,12 +4,7 @@ const port = 4000
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017', (err,db) => {
-  if (!err) {
-  console.log('Connected to MongoDB!');
-  }
-
-});
+mongoose.connect('mongodb+srv://LostKart:lostkart123@cluster0.d3b0w.mongodb.net/?retryWrites=true&w=majority')
 
 
 const User = mongoose.model('Users', { username: String, password: String });
@@ -19,8 +14,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('signup', (req, res) => {
-     const username = req.body.username;
-     const password = req.body.password;
+    // const username = req.body.username;
+     //const password = req.body.password;
      const user = new User({ username: 'usernameee', password: 'passworddd' });
      user.save().then(() => console.log('saved'));
   })
